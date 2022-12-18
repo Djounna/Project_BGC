2196aa89e04ebfd9219aabce28c89418a27f439d
Djounna <80339248+Djounna@users.noreply.github.com>
2022-12-18 15:56:33 +0100

    fix

--- BGC_Portal/src/app/app.module.ts
+++ BGC_Portal/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,6 +15,7 @@ import { ApiModule } from './api/api.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     ApiModule.forRoot({ rootUrl: 'https://localhost:7139' }),
     AuthModule.forRoot({
       domain : 'dev-c6lwemo7.us.auth0.com',
