7dc69e1cb85b251bc16bacc4d04e93828b765635
Djounna <80339248+Djounna@users.noreply.github.com>
2023-01-05 23:32:47 +0100

    Initial Commit

--- BGC_Portal/src/app/catalog/add-game/add-game.component.ts
+++ BGC_Portal/src/app/catalog/add-game/add-game.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { ɵInjectableAnimationEngine } from '@angular/platform-browser/animations';
+import { GameDto } from 'src/app/api/models';
+import { DialogData } from 'src/app/shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-add-game',
@@ -7,14 +15,28 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./add-game.component.css'],
 })
 export class AddGameComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder) {}
+  public newGame: GameDto = {};
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public dialogRef: MatDialogRef<AddGameComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: GameDto
+  ) {}
 
   ngOnInit(): void {}
 
   newGameForm = this.formBuilder.group({
-    Name: ['', Validators.required],
+    Name: ['', [Validators.required, Validators.maxLength(50)]],
     Description: ['', Validators.required],
     MinNumberPlayers: ['', Validators.required],
     MaxNumberPlayers: ['', Validators.required],
   });
+
+  saveNewGame(): void {
+    this.dialogRef.close(this.newGame);
+  }
+
+  closeDialog() {
+    this.dialogRef.close('Pizza!');
+  }
 }
--- BGC_Portal/src/app/catalog/catalog.component.ts
+++ BGC_Portal/src/app/catalog/catalog.component.ts
@@ -1,34 +1,42 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Observable, Subscription } from 'rxjs';
 import { GameDto } from '../api/models';
-import { GameService} from '../api/services';
+import { GameService } from '../api/services';
+import { AddGameComponent } from './add-game/add-game.component';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
-  styleUrls: ['./catalog.component.css']
+  styleUrls: ['./catalog.component.css'],
 })
 export class CatalogComponent implements OnInit, OnDestroy {
+  games!: GameDto[];
+  sub!: Subscription;
+  newGame: GameDto = {};
 
-  games! : GameDto[];
-  sub! : Subscription;
-
-
-  constructor(private gameService : GameService) { }
+  constructor(private gameService: GameService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
-    this.sub = this.gameService.apiGameGet$Json().subscribe(
-      {
-        next: (res) => 
-        this.games = res,
-        error: (e) => console.log('Error : ' + e.message),
-        complete: () => console.log('complete')
-      }
-    )
+    this.sub = this.gameService.apiGameGet$Json().subscribe({
+      next: (res) => (this.games = res),
+      error: (e) => console.log('Error : ' + e.message),
+      complete: () => console.log('complete'),
+    });
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.sub.unsubscribe();
   }
 
+  addGameDialog(): void {
+    const dialogRef = this.dialog.open(AddGameComponent, {
+      data: this.newGame,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.newGame = result;
+    });
+  }
 }
